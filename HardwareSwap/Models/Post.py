6b6c3de872e803f0001e980ce8ed0eec51b8e75f
Neil Dencklau <denck007@umn.edu>
2021-10-15 21:39:25 -0500

    Parsing title to location, have, want

--- HardwareSwap/Models/Post.py
+++ HardwareSwap/Models/Post.py
@@ -5,6 +5,7 @@ import pandas as pd
 import pytz
 import re
 import tqdm
+import sys
 from sqlalchemy.sql.schema import ForeignKey
 from sqlalchemy import Column, Integer, String, Float, DateTime, Boolean
 from sqlalchemy.orm import relation, relationship
@@ -19,6 +20,9 @@ class Post(Base):
     deleted = Column(Boolean, default=False, nullable=False)
     post_type_id = Column(Integer, ForeignKey("post_type.id"))
     post_type = relationship("PostType", back_populates="posts")
+    location = Column(String)
+    have = Column(String)
+    want = Column(String)
 
     all_awardings = Column(String)
     allow_live_comments = Column(Boolean)
@@ -189,6 +193,8 @@ class Post(Base):
     @classmethod
     def clean(cls, session):
         cls._mark_deleted(session)
+        cls._parse_title(session)
+        
         cls._set_post_type_by_link_flair(session)
         cls._set_post_type_by_parsing_title(session)
         
@@ -229,35 +235,57 @@ class Post(Base):
             post_type_ids[pt.post_type] = pt.id
         
         mapping = []
-        title_regex = get_regex_to_parse_title()
-        query = session.query(Post.id, Post.title).filter(Post.deleted==False, Post.post_type==None)
-        for post_id, title in query.all():
-            result = title_regex.match(title)
-            if result is None:
-                continue
-            result = result.groups()
-            if len(result) != 3:
-                continue
-            else:
-                _, have, want = result
-                have_clean = have.lower().replace(" ","")
-                want_clean = want.lower().replace(" ","")
-                
-                sell = ("paypal" in have_clean) or ("cash" in have_clean) or ("$" in have_clean)
-                buy = ("paypal" in want_clean) or ("cash" in want_clean) or ("$" in want_clean)
-                trade = "trade" in want_clean
-                if sell and not buy:
-                    mapping.append({"id":post_id, "post_type_id": post_type_ids["selling"]})
-                elif buy and not sell:
-                    mapping.append({"id":post_id, "post_type_id": post_type_ids["buying"]})
-                elif trade:
-                    mapping.append({"id":post_id, "post_type_id": post_type_ids["trading"]})
-                elif ("free" in want_clean) and ("sync" not in want_clean):
-                    mapping.append({"id":post_id, "post_type_id": post_type_ids["giveaway"]})
+        query = session.query(Post.id, Post.have, Post.want).filter(Post.deleted==False, Post.have!=None, Post.want!=None, Post.post_type_id!=None)
+        for post_id, have, want in query.all():
+            have_clean = have.lower().replace(" ","")
+            want_clean = want.lower().replace(" ","")
+            combined_clean = have_clean + want_clean
+            
+            buy = ("paypal" in have_clean) or ("cash" in have_clean) or ("$" in have_clean)
+            sell = ("paypal" in want_clean) or ("cash" in want_clean) or ("$" in want_clean)
+            trade = ("trade" in want_clean) or ( ("paypal" not in combined_clean) and ("cash" not in combined_clean) and ("$" not in combined_clean))
+            if sell and not buy:
+                mapping.append({"id":post_id, "post_type_id": post_type_ids["selling"]})
+            elif buy and not sell:
+                mapping.append({"id":post_id, "post_type_id": post_type_ids["buying"]})
+            elif trade:
+                mapping.append({"id":post_id, "post_type_id": post_type_ids["trading"]})
+            elif ("free" in want_clean) and ("sync" not in want_clean):
+                mapping.append({"id":post_id, "post_type_id": post_type_ids["giveaway"]})
 
         session.bulk_update_mappings(Post, mapping)
         session.commit()
 
+    @classmethod
+    def _parse_title(cls, session):
+        """
+        Parse the title of the post into location, have, want
+        """
+        title_regex = get_regex_to_parse_title()
+        query = session.query(Post.id, Post.title).filter(Post.deleted==False, Post.location==None)
+        mapping = []
+        for post_id, title in query.all():
+            try:
+                result = title_regex.match(title)
+                result = result.groups()
+                location, have, want = result
+                mapping.append(
+                    {
+                        "id":post_id,
+                        "location": location,
+                        "have": have,
+                        "want": want,
+                    }
+                )
+            except AttributeError as e:
+                # NoneType has no attribute groups is raised when the regex doesn't find anything
+                if len( re.findall(r".*NoneType.*object.*no attribute.*groups.*", str(sys.exc_info()[1])) ) == 1:
+                    pass
+                else:
+                    raise
+        session.bulk_update_mappings(Post, mapping)
+        session.commit()
+        
 
 def get_regex_to_parse_title():
     """
